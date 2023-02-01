373443b7b7fd8014bb189eb02de111e82424e88f
#check <hyunmail521@gmail.com>
2023-02-01 23:17:43 +0900

    next api 불러오기

---
+++ nextjs-blog/src/constants/userDetail.js
@@ -0,0 +1,6 @@
+export const userDetail = {
+  name: 'Hyunju LEE',
+  age: 12,
+  height: '100cm',
+  weigth: '100kg',
+}
---
+++ nextjs-blog/src/pages/[username]/[info].js
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export default function UsernameInfo() {
+  const router = useRouter()
+  const { username, info } = router.query
+  const [name, setName] = useState('?')
+
+  useEffect(() => {
+    fetch('/api/user')
+      .then((res) => res.json())
+      .then((data) => {
+        setName(data.name)
+      })
+  }, [])
+
+  return (
+    <>
+      <h1 className="title">
+        {username}'s {info}
+      </h1>
+      <h1 className="title">Name : {name}</h1>
+    </>
+  )
+}
+
+UsernameInfo.getLayout = function getLayout(page) {
+  return <>{page}</>
+}
